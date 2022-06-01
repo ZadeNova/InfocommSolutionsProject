0f5ee9f10bac8d75ecdaf7a58aa05b2d0a1ecdb2
hongjiNH <hongjichai@gmail.com>
2022-06-01 19:37:56 +0800

    cfm email link will be send to user email

--- InfocommSolutionsProject/Program.cs
+++ InfocommSolutionsProject/Program.cs
@@ -3,18 +3,23 @@ using Microsoft.EntityFrameworkCore;
 using InfocommSolutionsProject.Data;
 using InfocommSolutionsProject.Areas.Identity.Data;
 using InfocommSolutionsProject.Models;
+using InfocommSolutionsProject.Services;
 using System.Security.Claims;
 using System.Security.Policy;
-
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var connectionString = builder.Configuration.GetConnectionString("InfocommSolutionsProjectContextConnection") ?? throw new InvalidOperationException("Connection string 'InfocommSolutionsProjectContextConnection' not found.");
 System.Diagnostics.Debug.WriteLine(connectionString);
 builder.Services.AddDbContext<InfocommSolutionsProjectContext>(options =>
     options.UseSqlServer(connectionString)); ;
-
+//sending email :
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+// end 
 builder.Services.AddDefaultIdentity<Accounts>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<InfocommSolutionsProjectContext>(); ;
+
 
 // testing
 builder.Services.AddAuthorization(options => {
