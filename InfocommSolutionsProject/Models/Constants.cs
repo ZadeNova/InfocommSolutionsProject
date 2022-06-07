083a3cf8496ab7996c726c48dd1d469000c2ed97
hongjiNH <hongjichai@gmail.com>
2022-06-07 21:55:08 +0800

    added role base

---
+++ InfocommSolutionsProject/Models/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace InfocommSolutionsProject.Models
+{
+    public static class Roles
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+        public const string SGAE_STAFF = "SGAE_STAFF";
+       
+    }
+    
+}
--- InfocommSolutionsProject/Program.cs
+++ InfocommSolutionsProject/Program.cs
@@ -8,19 +8,59 @@ using System.Security.Claims;
 using System.Security.Policy;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using AspNetCore.ReCaptcha;
+using Microsoft.AspNetCore.Authentication.Google;
+
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("InfocommSolutionsProjectContextConnection") ?? throw new InvalidOperationException("Connection string 'InfocommSolutionsProjectContextConnection' not found.");
+
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+
+var connectionString = configuration.GetConnectionString("InfocommSolutionsProjectContextConnection") ?? throw new InvalidOperationException("Connection string 'InfocommSolutionsProjectContextConnection' not found.");
 System.Diagnostics.Debug.WriteLine(connectionString);
-builder.Services.AddDbContext<InfocommSolutionsProjectContext>(options =>
+services.AddDbContext<InfocommSolutionsProjectContext>(options =>
     options.UseSqlServer(connectionString)); ;
 //sending email :
-builder.Services.AddTransient<IEmailSender, EmailSender>();
+services.AddTransient<IEmailSender, EmailSender>();
 // end 
-builder.Services.AddDefaultIdentity<Accounts>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
+services.AddDefaultIdentity<Accounts>(options => options.SignIn.RequireConfirmedAccount = true).AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<InfocommSolutionsProjectContext>(); ;
 
-builder.Services.AddReCaptcha(builder.Configuration.GetSection("ReCaptcha"));
+services.AddReCaptcha(configuration.GetSection("ReCaptcha"));
+services.AddAuthentication()
+    .AddGoogle(GoogleOptions => {
+        GoogleOptions.ClientId = configuration["Google:ClientId"];
+        GoogleOptions.ClientSecret = configuration["Google:ClientSecret"];
+      
+    });
+services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdministratorRole",
+         policy => policy.RequireRole("Admin"));
+    options.AddPolicy("RequireCustomerRole",
+        policy => policy.RequireRole("Customer"));
+    options.AddPolicy("RequirStaffRole",
+       policy => policy.RequireRole("SGAE_STAFF"));
+    options.AddPolicy("RequirbothRole",
+      policy => policy.RequireRole("SGAE_STAFF" , "Admin"));
+  
+});
+
+services.AddRazorPages(options =>
+{
+   
+    options.Conventions.AuthorizeFolder("/Supplier", "RequirbothRole");
+    options.Conventions.AuthorizeFolder("/SupplierOrders", "RequirbothRole");
+    options.Conventions.AuthorizePage("/Product/Delete", "RequireAdministratorRole");
+    options.Conventions.AuthorizePage("/Product/Edit", "RequireAdministratorRole");
+    options.Conventions.AuthorizePage("/Product/Create", "RequireAdministratorRole");
+
+    //options.Conventions.AuthorizePage("/Contact");
+    //options.Conventions.AuthorizeFolder("/Private");
+    //options.Conventions.AllowAnonymousToPage("/Private/PublicPage");
+    //options.Conventions.AllowAnonymousToFolder("/Private/PublicPages");
+});
 // testing
 //builder.Services.AddAuthorization(options => {
 //    //options.AddPolicy("Admin", policy => policy.RequireClaim("IamAdmin"));
@@ -28,22 +68,24 @@ builder.Services.AddReCaptcha(builder.Configuration.GetSection("ReCaptcha"));
 //});
 
 // Add services to the container.
-builder.Services.AddRazorPages();
+services.AddRazorPages();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+
     app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication(); ;
-
+app.UseAuthentication(); 
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
