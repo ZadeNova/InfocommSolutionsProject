9457a36f077f6594fd1d20644d9612babb5d3b8e
hongjiNH <hongjichai@gmail.com>
2022-07-01 20:33:53 +0800

    added pagination + search on categpry and supplier

--- InfocommSolutionsProject/Pages/Category/Index.cshtml.cs
+++ InfocommSolutionsProject/Pages/Category/Index.cshtml.cs
@@ -13,19 +13,40 @@ namespace InfocommSolutionsProject.Pages.Category
     public class IndexModel : PageModel
     {
         private readonly InfocommSolutionsProject.Data.InfocommSolutionsProjectContext _context;
-
-        public IndexModel(InfocommSolutionsProject.Data.InfocommSolutionsProjectContext context)
+        private readonly IConfiguration Configuration;
+        public IndexModel(InfocommSolutionsProject.Data.InfocommSolutionsProjectContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
+        public string CurrentFilter { get; set; }
+        public PaginatedList<Categories> categore { get; set; }
+        //public IList<Categories> Categories { get;set; } = default!;
 
-        public IList<Categories> Categories { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchString, int? pageIndex)
         {
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = CurrentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<Categories> CateQueryable = from P in _context.Categories select P;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                CateQueryable = CateQueryable.Where(s => s.CategoryName.Contains(searchString) || s.CategoryFor.Contains(searchString) || s.Description.Contains(searchString));
+            }
             if (_context.Categories != null)
             {
-                Categories = await _context.Categories.ToListAsync();
+                var pageSize = Configuration.GetValue("PageSize", 10);
+                categore = await PaginatedList<Categories>.CreateAsync(CateQueryable.AsNoTracking(), pageIndex ?? 1, pageSize);
+                //Categories = await _context.Categories.ToListAsync();
             }
         }
     }
--- InfocommSolutionsProject/Pages/Supplier/Index.cshtml.cs
+++ InfocommSolutionsProject/Pages/Supplier/Index.cshtml.cs
@@ -13,19 +13,41 @@ namespace InfocommSolutionsProject.Pages.Supplier
     public class IndexModel : PageModel
     {
         private readonly InfocommSolutionsProject.Data.InfocommSolutionsProjectContext _context;
-
-        public IndexModel(InfocommSolutionsProject.Data.InfocommSolutionsProjectContext context)
+        private readonly IConfiguration Configuration;
+        public IndexModel(InfocommSolutionsProject.Data.InfocommSolutionsProjectContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
-        public IList<SupplierModel> SupplierModel { get;set; } = default!;
+       // public IList<SupplierModel> SupplierModel { get;set; } = default!;
+        public string CurrentFilter { get; set; }
+        public PaginatedList<SupplierModel> suppler { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string searchString, int? pageIndex)
         {
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = CurrentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<SupplierModel> SupplierQueryable = from P in _context.Suppliers select P;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                SupplierQueryable = SupplierQueryable.Where(s => s.SupplierName.Contains(searchString) || s.SupplierCategory.Contains(searchString));
+            }
             if (_context.Suppliers != null)
             {
-                SupplierModel = await _context.Suppliers.ToListAsync();
+                var pageSize = Configuration.GetValue("PageSize", 10);
+                suppler = await PaginatedList<SupplierModel>.CreateAsync(SupplierQueryable.AsNoTracking(), pageIndex ?? 1, pageSize);
+                //SupplierModel = await _context.Suppliers.ToListAsync();
             }
         }
     }
