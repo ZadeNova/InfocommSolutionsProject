93756bdb4295f52790eaa3bbca65c3b879d183bc
Inquisitives <inquisitivesosu@gmail.com>
2022-07-05 10:27:47 +0800

    implement location services (currently not fully working)

---
+++ InfocommSolutionsProject/Pages/Location.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InfocommSolutionsProject.Pages
+{
+    public class LocationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
