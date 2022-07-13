603b527110953a64c38918813e91b153b64b7c19
Zade <muhderfannnn@gmail.com>
2022-07-14 01:47:47 +0800

    Made some IOT dashboard changes - Zade

--- InfocommSolutionsProject/wwwroot/js/AdminPageJs/AdminPageCharts/TempChart.js
+++ InfocommSolutionsProject/wwwroot/js/AdminPageJs/AdminPageCharts/TempChart.js
@@ -6,8 +6,14 @@
         legendState = false;
     }
     var speed = 250;
+    // Creation of Data to be used in the chart
     var TempData = [];
     var DateTempData = [];
+    var HumidityData = [];
+    var DateHumidityData = [];
+    var LightData = [];
+    var DateLightData = [];
+
     //TempData.length = 100;
     //DateTempData.length = 100;
     //TempData.fill(0);
@@ -85,9 +91,149 @@
         }
     })
 
+    var TheHumiditychart = new Chart(document.getElementById("HumidityChart"), {
+        type: "line",
+        options: {
 
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Real Time Humidity Chart',
+                    fontColor: 'white',
+                }
+            },
+            scales: {
+                xAxes: [
+                    {
+                        display: true,
+                        gridLines: {
+                            display: false,
+                        },
+                    },
+                ],
+                yAxes: [
+                    {
+                        ticks: {
+                            max: 60,
+                            min: 10,
+                        },
+                        display: true,
+                        gridLines: {
+                            display: false,
+                        },
+                    },
+                ],
+            },
+            legend: {
+                display: legendState,
+            },
+            responsive: true,
+            animation: {
+                duration: speed * 1.5,
+                easing: 'linear'
+            },
+        },
+        data: {
+            labels: DateHumidityData,
+            datasets: [
+                {
+                    label: "Humidity",
+                    fill: true,
+                    lineTension: 0.2,
+                    backgroundColor: "transparent",
+                    borderColor: "#57DECE",
+                    pointBorderColor: "#57DECE",
+                    pointHoverBackgroundColor: "#57DECE",
+                    borderCapStyle: "butt",
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: "miter",
+                    borderWidth: 2,
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 5,
+                    pointHoverRadius: 5,
+                    pointHoverBorderColor: "#fff",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 0,
+                    data: HumidityData,
+                    spanGaps: false,
+                }
+            ]
+        }
+    })
     
+    var TheLightchart = new Chart(document.getElementById("LightChart"), {
+        type: "line",
+        options: {
 
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Real Time Light Chart',
+                    fontColor: 'white',
+                }
+            },
+            scales: {
+                xAxes: [
+                    {
+                        display: true,
+                        gridLines: {
+                            display: false,
+                        },
+                    },
+                ],
+                yAxes: [
+                    {
+                        ticks: {
+                            max: 60,
+                            min: 10,
+                        },
+                        display: true,
+                        gridLines: {
+                            display: false,
+                        },
+                    },
+                ],
+            },
+            legend: {
+                display: legendState,
+            },
+            responsive: true,
+            animation: {
+                duration: speed * 1.5,
+                easing: 'linear'
+            },
+        },
+        data: {
+            labels: DateLightData,
+            datasets: [
+                {
+                    label: "Light",
+                    fill: true,
+                    lineTension: 0.2,
+                    backgroundColor: "transparent",
+                    borderColor: "#FE9360",
+                    pointBorderColor: "#FE9360",
+                    pointHoverBackgroundColor: "#FE9360",
+                    borderCapStyle: "butt",
+                    borderDash: [],
+                    borderDashOffset: 0.0,
+                    borderJoinStyle: "miter",
+                    borderWidth: 2,
+                    pointBackgroundColor: "#fff",
+                    pointBorderWidth: 5,
+                    pointHoverRadius: 5,
+                    pointHoverBorderColor: "#fff",
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 0,
+                    data: LightData,
+                    spanGaps: false,
+                }
+            ]
+        }
+    })
     
      
 
@@ -101,16 +247,29 @@
             console.log(TheData["DateTime"])
             DateTempData.push(TheData["DateTime"])
             TempData.push(TheData["temperature"])
-            //TempData.shift();
-            //DateTempData.shift();
+            DateHumidityData.push(TheData["DateTime"])
+            HumidityData.push(TheData["humidity"])
+            LightData.push(TheData["light"])
+            DateLightData.push(TheData["DateTime"])
+            console.log(LightData);
             TheTempchart.update();
-            console.log(TempData.length)
-            console.log(DateTempData.length)
-            if (TempData.length > 15 && DateTempData.length > 15) {
+            TheHumiditychart.update();
+            TheLightchart.update();
+
+            // This if statement is used to remove previous data points. Currently chart can only hold 7 data points due to the if statement.
+            if (TempData.length > 7 && DateTempData.length > 7) {
                 TempData.shift();
                 DateTempData.shift();
+                HumidityData.shift();
+                DateHumidityData.shift();
+                LightData.shift()
+                DateLightData.shift()
                 console.log("Removing in progress")
             }
+
+            //if (HumidityData.length > 7 && DateHumidityData > 7) {
+
+            //}
            
         });
 
