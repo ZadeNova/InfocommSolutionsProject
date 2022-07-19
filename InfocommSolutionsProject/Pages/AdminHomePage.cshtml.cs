51efff900a8081d427fde894a95714a32e955f66
Zade <muhderfannnn@gmail.com>
2022-07-20 01:18:35 +0800

    Added Date filtering for User chart. Now Date filters can be used for any chart as I have the code for it. - Zade.

--- InfocommSolutionsProject/Pages/AdminHomePage.cshtml.cs
+++ InfocommSolutionsProject/Pages/AdminHomePage.cshtml.cs
@@ -9,6 +9,8 @@ using InfocommSolutionsProject.Data;
 using InfocommSolutionsProject.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json;
 
 namespace InfocommSolutionsProject.Pages
 {
@@ -16,9 +18,12 @@ namespace InfocommSolutionsProject.Pages
     {
         private readonly InfocommSolutionsProject.Data.InfocommSolutionsProjectContext _context;
 
-        public AdminPageModel(InfocommSolutionsProjectContext context)
+        private readonly UserManager<Accounts> _userManager;
+
+        public AdminPageModel(InfocommSolutionsProjectContext context,UserManager<Accounts> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public int NumberOfProducts { get; set; }
@@ -29,12 +34,24 @@ namespace InfocommSolutionsProject.Pages
 
         public int NumberOfProductOrders { get; set; }
 
+        public int NumberOfUsersInWebApplication { get; set; }
+
+        public IEnumerable<DateTime>? UserDateList { get; set; }
+
         public void OnGet()
         {
             NumberOfProducts = _context.Products.Count();
             NumberOfSuppliers = _context.Suppliers.Count();
             NumberOfProductOrders = _context.Orders.Count();
             NumberOfSupplierOrders = _context.SupplierOrders.Count();
+            NumberOfUsersInWebApplication = _userManager.Users.Count();
+
+            var UserList = _userManager.Users.ToList();
+            var testvar = from a in _userManager.Users select a.CreatedAt;
+            UserDateList = UserList.Select(a => a.CreatedAt);
+            
+            //foreach (var i in UserList) System.Diagnostics.Debug.WriteLine($"{i} wtfffff");
+            //foreach (var i in UserDates) System.Diagnostics.Debug.WriteLine($"{i} fuck u");
 
             //System.Diagnostics.Debug.WriteLine($"{NumberOfProducts} HZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZ");
 
