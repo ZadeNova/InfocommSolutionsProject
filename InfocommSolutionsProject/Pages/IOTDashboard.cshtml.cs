861a89e7e19c9f4adfb2c52ae2eaaaa1c09f1072
hongjiNH <hongjichai@gmail.com>
2022-07-20 03:25:03 +0800

    when admin clik IOT page it will auto show the diagram

--- InfocommSolutionsProject/Pages/IOTDashboard.cshtml.cs
+++ InfocommSolutionsProject/Pages/IOTDashboard.cshtml.cs
@@ -2,7 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
-
+using System.Text;
+using System.Net;
 
 namespace InfocommSolutionsProject.Pages
 {
@@ -10,7 +11,20 @@ namespace InfocommSolutionsProject.Pages
     {
         public void OnGet()
         {
-            
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("https://infocommsolutionsprojectsgae.scm.azurewebsites.net/api/triggeredwebjobs/IOTSim/run");
+            request.Method = "POST";
+            var byteArray = Encoding.ASCII.GetBytes("$InfocommSolutionsProjectSGAE:njTfurAvwCtcJtDamfdbTL4njqczqyicBdEbN7G3lZurQDxfbjZ5kSQvx2WM"); //we could find user name and password in Azure web app publish profile 
+            request.Headers.Add("Authorization", "Basic " + Convert.ToBase64String(byteArray));
+            request.ContentLength = 0;
+           
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+            }
+            catch (Exception e)
+            {
+
+            }
         }
     }
 }
