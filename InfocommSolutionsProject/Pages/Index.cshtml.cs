efe23aea049f60ecfd2860da40fce5509a10f73c
hongjiNH <hongjichai@gmail.com>
2022-07-29 14:51:54 +0800

    change the customer layout to display products before login

--- InfocommSolutionsProject/Pages/Index.cshtml.cs
+++ InfocommSolutionsProject/Pages/Index.cshtml.cs
@@ -1,20 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using InfocommSolutionsProject.Data;
+
+using Microsoft.EntityFrameworkCore;
+using InfocommSolutionsProject.Models;
 
 namespace InfocommSolutionsProject.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly InfocommSolutionsProject.Data.InfocommSolutionsProjectContext _context;
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public IList<ProductModel> Products { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, InfocommSolutionsProjectContext context)
         {
             _logger = logger;
-        }
+            _context = context;
 
+        }
+       
         public void OnGet()
         {
-
+            Products = _context.Products.ToList();
+            foreach (var item in Products) System.Diagnostics.Debug.WriteLine(item.Name);
         }
     }
 }
